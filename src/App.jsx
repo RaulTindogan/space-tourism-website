5efbe56872756aad089c40a958879debfecc4a06
RaulTindogan <tindoganraul@gmail.com>
2024-03-31 21:56:20 +0800

    Edited Links

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home'
 import Page404 from './pages/Page404'
 import ScrollToTop from './components/ScrollToTop'
 import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
+import Destination from './pages/Destination'
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <ScrollToTop />
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/destination' element={<Destination />} />
           <Route path='404' element={<Page404 />} />
           <Route path='*' element={<Navigate to='404' replace />}  />
         </Routes>
