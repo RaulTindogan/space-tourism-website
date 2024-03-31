d9a5058d567c654050d5e8b628b3488f167ee99f
RaulTindogan <tindoganraul@gmail.com>
2024-03-31 21:55:33 +0800

    Created Destination Page and add background

---
+++ src/pages/Destination.jsx
@@ -0,0 +1,17 @@
+import data from '../assets/data.json'
+
+function Destination() {
+  return (
+    <main 
+        className={`
+            bg-[url('src/assets/destination/background-destination-desktop.jpg')]
+            h-[100vh] 
+            `}
+
+    >
+
+    </main> 
+  )
+}
+
+export default Destination
