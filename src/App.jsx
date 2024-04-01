4da924a261670da62ae89da324b3b8bbfd456938
RaulTindogan <tindoganraul@gmail.com>
2024-04-01 20:23:54 +0800

    Add Route for Crew Page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Page404 from './pages/Page404'
 import ScrollToTop from './components/ScrollToTop'
 import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
 import Destination from './pages/Destination'
+import Crew from './pages/Crew'
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/destination' element={<Destination />} />
+          <Route path='/crew' element={<Crew />} />
           <Route path='404' element={<Page404 />} />
           <Route path='*' element={<Navigate to='404' replace />}  />
         </Routes>
