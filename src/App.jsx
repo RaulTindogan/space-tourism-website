77541ff20116762da7b7ad4a0080a84fecfe23ee
RaulTindogan <tindoganraul@gmail.com>
2024-04-02 21:39:14 +0800

    Added Technology.jsx to Route

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import ScrollToTop from './components/ScrollToTop'
 import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
 import Destination from './pages/Destination'
 import Crew from './pages/Crew'
-
+import Technology from './pages/Technology'
 
 function App() {
 
@@ -18,6 +18,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/destination' element={<Destination />} />
           <Route path='/crew' element={<Crew />} />
+          <Route path='/technology' element={<Technology />} />
           <Route path='404' element={<Page404 />} />
           <Route path='*' element={<Navigate to='404' replace />}  />
         </Routes>
