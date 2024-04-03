2018dc9678558e5099b68436574935bc832b0444
RaulTindogan <tindoganraul@gmail.com>
2024-04-03 21:19:03 +0800

    Edited the reponsiveness and other styles

--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -21,22 +21,24 @@ function Destination() {
             bg-cover
             bg-no-repeat
             min-h-[100vh] 
-            pt-[10rem]
+            pt-[7rem]
             pb-10
             text-white
             px-5
-            sm:px-[10rem]
+            sm:px-10
+            lg:pt-[10rem]
+            lg:flex
             `}
     >
-      <section className='flex flex-col justify-center mb-5 bg-no'>
-        <h1 className="text-xl text-center mb-10"><span>01</span> Pick your destination</h1>
+      <section className='flex flex-col justify-center mb-10 lg:w-[40%] lg:justify-start'>
+        <h1 className="text-2xl text-center mb-10 md:text-left"><span>01</span> Pick your destination</h1>
         <div className='flex justify-center items-center'>
-          <img src={displayData.images.webp} alt="Moon" className='w-3/4 h-3/4'/>
+          <img src={displayData.images.webp} alt="Moon" className='w-3/4 h-3/4 sm:w-1/2 sm:h-1/2 lg:w-full lg:h-full'/>
         </div>
       </section>
-      <section>
+      <section className='sm:px-[5rem] lg:w-[60%] lg:pt-[5rem]'>
         <nav>
-          <ul className='flex gap-x-5 justify-center'>
+          <ul className='flex gap-x-5 justify-center lg:justify-start lg:gap-x-10'>
             {
               destinationLinks.map((link, index)=>(
                 <li key={index} onClick={()=>{activeData(index), handleActiveItem(link)}} className={`cursor-pointer ${activeItem===link? ' text-[red]' : ''}`}>{link}</li>
@@ -45,12 +47,12 @@ function Destination() {
           </ul>
         </nav>
         <div>
-          <div className='text-center'>
-            <h2 className='text-[5rem] my-5'>{displayData.name}</h2>
-            <p>{displayData.description}</p>
+          <div className='text-center lg:text-left'>
+            <h2 className='font-bellefair uppercase text-[5rem] my-5'>{displayData.name}</h2>
+            <p className=''>{displayData.description}</p>
           </div>
          <hr className='my-10'/>
-         <div className='text-center'>
+         <div className='text-center lg:flex lg:gap-[5rem]'>
           <div className='mb-5'>
             <p>Avg. distance</p>
             <h3 className='text-3xl'>{displayData.distance}</h3>
