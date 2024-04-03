3273a756c05fb2eb572038704a490aca5c3bec06
RaulTindogan <tindoganraul@gmail.com>
2024-04-03 21:15:55 +0800

    Create active state of mobile navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,42 +1,44 @@
 import React, { useState } from 'react'
 import logo from '../assets/shared/logo.svg'
 import menu from '../assets/shared/icon-hamburger.svg'
-import { Link } from 'react-router-dom'
+import closeMenu from '../assets/shared/icon-close.svg'
+import { NavLink } from 'react-router-dom'
 function Header() {
+  const [activeNav, setactiveNav] = useState(false)
   return (
       <header className='bg-transparent fixed w-full'>
-        <nav className='max-w-[1440px] mx-auto relative z-20'>
+        <nav className='max-w-[1440px] mx-auto relative z-20 lg:mt-10'>
           {/* Mobile Menu */}
             <div className='p-5 flex justify-between items-center md:hidden'>
               <div>
                   <a href="#"><img src={logo} alt="LOGO" /></a>
               </div>
-              <div>
-                <a href="#"><img src={menu} alt="" /></a>
+              <div onClick={()=>{setactiveNav(!activeNav)}}>
+                <button><img src={!activeNav? menu : closeMenu} alt="" /></button>
               </div>
             </div>
           {/* Desktop Menu */}
             <div className='hidden md:flex justify-between items-center text-white pl-10 relative'>
-              <div className=''>
+              <div>
                   <a href="#"><img src={logo} alt="LOGO" /></a>
               </div>
               <hr className='hidden lg:block w-[35%] h-[1px] absolute left-[8rem] z-10'/>
               <div className='lg:w-[60%]'>
-                <ul className='flex gap-10 bg-black p-10 lg:justify-center xl:gap-[5rem]'>
-                  <li><Link to="/">HOME</Link></li>
-                  <li><Link to="/destination">DESTINATION</Link></li>
-                  <li><Link to="/crew">CREW</Link></li>
-                  <li><Link to="/technology">TECHNOLOGY</Link></li>
+                <ul className='flex gap-10 bg-white/5 backdrop-blur-lg p-8 lg:justify-center xl:gap-[5rem]'>
+                  <li><NavLink to="/">HOME</NavLink></li>
+                  <li><NavLink to="/destination">DESTINATION</NavLink></li>
+                  <li><NavLink to="/crew">CREW</NavLink></li>
+                  <li><NavLink to="/technology">TECHNOLOGY</NavLink></li>
                 </ul>
               </div>
             </div>
         </nav>     
-        <div className='w-[60%] min-h-[100vh] fixed z-[10] bg-black/50 top-0 right-0 overflow-hidden text-white py-[7rem] px-[3rem]'>
+        <div className={`w-[60%] min-h-[100vh] fixed z-[10] bg-black/50 top-0 right-0 overflow-hidden text-white py-[7rem] px-[3rem] ${activeNav?'':' hidden'}`}>
             <ul>
-              <li className='mb-10 py-2 border-b-[3px]'><Link to="/">00 HOME</Link></li>
-              <li className='mb-10 py-2 border-b-[3px]'><Link to="/destination">01 DESTINATION</Link></li>
-              <li className='mb-10 py-2 border-b-[3px]'><Link to="/crew">02 CREW</Link></li>
-              <li className='mb-10 py-2 border-b-[3px]'><Link to="/technology">03 TECHNOLOGY</Link></li>
+              <li className='mb-10 py-2 border-b-[3px]'><NavLink to="/">00 HOME</NavLink></li>
+              <li className='mb-10 py-2 border-b-[3px]'><NavLink to="/destination">01 DESTINATION</NavLink></li>
+              <li className='mb-10 py-2 border-b-[3px]'><NavLink to="/crew">02 CREW</NavLink></li>
+              <li className='mb-10 py-2 border-b-[3px]'><NavLink to="/technology">03 TECHNOLOGY</NavLink></li>
             </ul>
         </div>
       </header>
